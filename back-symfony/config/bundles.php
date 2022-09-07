0811d4c3b06c9fa4ed4d640a35c70556676406dd
AidounNejma <guermachenejma@gmail.com>
2022-09-07 13:45:27 +0200

    installation webpack config

--- back-symfony/config/bundles.php
+++ back-symfony/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
